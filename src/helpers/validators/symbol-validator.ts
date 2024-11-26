f729cfc3387b7dc9535dbb14af5848875c7c2479
Ben <benhayward.ben@gmail.com>
2024-11-26 22:37:30 +0000

    Adding doc block

--- src/helpers/validators/symbol-validator.ts
+++ src/helpers/validators/symbol-validator.ts
@@ -1,3 +1,9 @@
+/**
+ * Validate symbol. Will throw an error if the value is not a valid symbol.
+ * @param { string } value - The value to validate.
+ * @throws { Error } - If the value is not a valid symbol.
+ * @returns { boolean } - True if valid.
+ */
 export function validateSymbol(value: string): boolean {
   if (value.length < 3) {
     throw new Error('Symbol must be at least 3 characters long');
